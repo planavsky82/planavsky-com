0336dbfbc2b2e139801095c056feeb8d38f44f4b
Anthony Planavsky <planavsky82@gmail.com>
2018-07-27 04:32:13 -0400

    edit build scripts

--- scripts/finalize.sh
+++ scripts/finalize.sh
@@ -20,6 +20,8 @@ if [$TRAVIS_PULL_REQUEST_BRANCH -eq '']
 	git status
 	git show --name-only
   echo "START PUSH."
+  git add package.json
+  git commit -m 'Upgrade to %s + patch for next version [ci skip]'
 	git push -f -q origin staging
   git push -q origin --tags
 	#echo 'START SERVER DEPLOYMENT.';
