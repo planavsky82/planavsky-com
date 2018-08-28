4cb4fc4b439c4fd34b30fe4cb6408b8b3a6c6de3
Anthony Planavsky <planavsky82@gmail.com>
2018-08-27 22:20:18 -0400

    add angular fontawesome icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,20 @@ import { NavigationModule } from 'ultimate-list';
 
 import { AppComponent } from './app.component';
 import { MainModule } from './main/main.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faAngular, faGithub, faJs, faCss3Alt, faHtml5, 
+  faNodeJs, faJenkins } from '@fortawesome/free-brands-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faAngular);
+library.add(faGithub);
+library.add(faJs);
+library.add(faCss3Alt);
+library.add(faHtml5);
+library.add(faNodeJs);
+library.add(faJenkins);
+library.add(faCoffee);
 
 @NgModule({
   declarations: [
@@ -13,6 +27,7 @@ import { MainModule } from './main/main.module';
   imports: [
     BrowserModule,
     MainModule,
+    FontAwesomeModule,
     NavigationModule
   ],
   providers: [],
