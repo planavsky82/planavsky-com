267963c02f90db676c7beb9d0f4812f288b1aad7
Anthony Planavsky <planavsky82@gmail.com>
2019-02-10 12:21:32 -0500

    setup

--- scripts/setup-api.sh
+++ scripts/setup-api.sh
@@ -5,14 +5,19 @@
 # var compression = require('compression');
 # var helmet = require('helmet');
 echo 'api ...'
+
+# as root: curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+# nvm install node for the ssh user
+
 cd ..
 cd api
 rm -rf node_modules
 npm i
 echo 'deleting current files ...'
-ssh planavsky_ftp@192.169.200.149 'rm -rf api'
-ssh planavsky_ftp@192.169.200.149 'mkdir api'
+ssh planavsky_ftp@192.169.200.149 'rm -rf planavsky.com/api'
+ssh planavsky_ftp@192.169.200.149 'mkdir planavsky.com/api'
 echo 'copying new files ...'
 for i in $(ls); 
-do scp -r $i planavsky_ftp@192.169.200.149:/var/www/planavsky.com;
+do scp -r $i planavsky_ftp@192.169.200.149:/var/www/planavsky.com/api;
 done;
