578a191c9d5e29ad22725ea002c55227d6dfdbf4
Anthony Planavsky <anthonyplanavsky@Anthonys-MacBook-Pro.local>
2020-02-12 12:36:40 -0500

    api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as express from 'express';
+import * as cors from 'cors';
 const app: express.Application = express();
 const port = 3000;
 
@@ -40,11 +41,15 @@ app.get('/user', (req, res) => {
     const db = admin.database();
     const ref = db.ref('/users/data/user1');
     ref.on('value', function(snapshot: any) {
+      return cors()(req, res, () => {
         res.send(snapshot);
+      });
     });
 });
 
 exports.app = functions.https.onRequest(app);
+
+// https://stackoverflow.com/questions/42755131/enabling-cors-in-cloud-functions-for-firebase
 
 // https://medium.com/@sandun.isuru/how-to-deploy-nodejs-express-app-to-firebase-as-function-31515c304e70
 
