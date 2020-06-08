aec02f88318015ef2e7f38cee324658c3d9dabb8
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP.lan>
2020-06-08 00:28:50 -0400

    api/angular integration

--- angular/projects/mffr/src/app/home/home.component.ts
+++ angular/projects/mffr/src/app/home/home.component.ts
@@ -24,12 +24,12 @@ export class HomeComponent {
   }
 
   getConfig() {
-    console.log(this.http.post<any>('https://us-central1-planavsky-com.cloudfunctions.net/app/authenticate',
+    this.http.post<any>('https://us-central1-planavsky-com.cloudfunctions.net/app/authenticate',
       { name: 'U10133', pwd: 'e3$f!rt78UNml90!' }, this.httpOptions)
       .subscribe((data: any) => {
-        console.log(this.http.get<any>('https://us-central1-planavsky-com.cloudfunctions.net/app/rankings',
+        this.http.get<any>('https://us-central1-planavsky-com.cloudfunctions.net/app/rankings',
         { params: { 'token': data.token } })
-        .subscribe((data: any) => console.log(data)));
-      }));
+        .subscribe((data: any) => console.log(data));
+      });
   }
 }
