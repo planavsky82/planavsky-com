60be6e1915168aa90aebc31d69fe5322b86a83a2
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP.lan>
2020-06-10 18:45:36 -0400

    add blog and update deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,9 @@
 cd angular
 npm run build:mffr
 cd ..
+cd blog
+npm run build
+cd ..
 rm -rf dist
 mkdir -p dist
 cd angular
@@ -12,4 +15,8 @@ mkdir -p web-components
 cd ../../stencil-web-components
 npm run build
 mv dist ../dist/mffr/web-components
+cd ../dist/mffr
+mkdir -p blog
+cd ../../blog
+mv dist ../dist/mffr/blog
 firebase deploy
