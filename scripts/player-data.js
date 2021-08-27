31ddb9a0830c550d11fb652519461d35acb19eb5
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP.lan>
2021-08-27 13:46:33 -0400

    player data

--- scripts/player-data.js
+++ scripts/player-data.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-axios.get('https://www.footballdb.com/players/index.html')
+axios.get('https://www.pro-football-reference.com/fantasy/RB-fantasy-matchups.htm')
   .then(response => {
     const html = response.data;
     console.log(html);
-  })
+  });
