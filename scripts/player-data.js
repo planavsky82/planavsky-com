78aa66b79683aa2aad64d55bab5cd6e3eb69d198
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP.lan>
2021-09-02 22:24:31 -0400

    player data

--- scripts/player-data.js
+++ scripts/player-data.js
@@ -5,7 +5,10 @@ const parse = require('node-html-parser').parse;
 axios.get('https://www.pro-football-reference.com/fantasy/RB-fantasy-matchups.htm')
   .then(response => {
     const html = parse(response.data);
-    // get only the body tag on the html const
-    const root = parse('<ul id="list"><li>Hello World</li></ul>');
-    console.log(root.querySelector('#list'));
+    // get only the body tag and its children from the html const
+    const body = parse('<body' + response.data.split('<body')[1].split('</body>')[0] + '</body>');
+    //const root = parse('<div><div><div><div><ul id="list"><li>Hello World</li></ul></div></div></div></div>');
+    //console.log(root.querySelector('#list'));
+    console.log(body);
+    //console.log(html.querySelector('#fantasy_stats'));
   });
