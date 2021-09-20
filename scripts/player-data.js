94cd939d86143a5c992ab1af587ef64fcf2f69b2
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP.lan>
2021-09-20 17:37:26 -0400

    get player data

--- scripts/player-data.js
+++ scripts/player-data.js
@@ -1,7 +1,40 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
+const fs = require('fs');
 
 const positions = ['QB', 'RB', 'WR', 'TE'];
+
+let playerData = {
+  position: [
+    {
+      type: 'qb',
+      players: [
+        {
+          id: ''
+        }
+      ]
+    }
+  ]
+};
+
+// json data
+var jsonData = '{"persons":[{"name":"John","city":"New York"},{"name":"Phil","city":"Ohio"}]}';
+
+// parse json
+var jsonObj = JSON.parse(jsonData);
+console.log(jsonObj);
+
+// stringify JSON Object
+var jsonContent = JSON.stringify(jsonObj);
+console.log(jsonContent);
+
+fs.writeFile("player-data.json", jsonContent, 'utf8', function (err) {
+    if (err) {
+      console.log("An error occured while writing JSON Object to File.");
+      return console.log(err);
+    }
+
+    console.log("JSON file has been saved.");
+});
 
 let getPlayers = (position) => {
   axios.get('https://www.pro-football-reference.com/fantasy/' + position + '-fantasy-matchups.htm')
