eae2a82d340b8ff26a8772a0d71518678203c0aa
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP.lan>
2021-10-09 17:15:46 -0400

    update data file

--- scripts/player-data.js
+++ scripts/player-data.js
@@ -3,25 +3,35 @@
 const axios = require('axios');
 const fs = require('fs');
 
-const positions = ['QB', 'RB', 'WR', 'TE', 'K'];
+const positions = ['QB', 'RB', 'WR', 'TE'];
+let existingData, positionData;
+
+try {
+  if (fs.existsSync('player-data.json')) {
+    existingData = fs.readFileSync('player-data.json', 'utf8') || true;
+    positionData = JSON.parse(existingData).positions;
+  }
+} catch(err) {
+  console.error('File does not yet exist');
+}
 
 let playerJSON = {
   positions: [
     {
       type: positions[0],
-      players: []
+      players: positionData ? positionData[0].players : []
     },
     {
       type: positions[1],
-      players: []
+      players: positionData ? positionData[1].players : []
     },
     {
       type: positions[2],
-      players: []
+      players: positionData ? positionData[2].players : []
     },
     {
       type: positions[3],
-      players: []
+      players: positionData ? positionData[3].players : []
     },
     {
       type: 'DST',
@@ -82,31 +92,39 @@ let getPlayers = (position) => {
     })
     rows.forEach((playerData, index) => {
       let playerDataSplit = playerData.split('.htm">');
-      console.log('----------------------');
+
       try {
         team = rows[index + 1].split('/teams/')[1].split('/')[0];
-        console.log(team);
       }
       catch(err) {
-        console.log('no data');
+        return true;
       }
-      let player = playerDataSplit[playerDataSplit.length - 1];
+      const player = playerDataSplit[playerDataSplit.length - 1];
       if (index !== (rows.length - 1)) {
-        console.log(player);
-        playerJSON.positions[positionIndex].players.push({
-          name: player,
-          id: (player + '-' + position).toLowerCase().trim().replace(' ', '-', /g/),
-          team: team.toUpperCase()
+        const playerId = (player + '-' + position).toLowerCase().trim().replace(' ', '-', /g/);
+        // check if player already exists
+        let exists = playerJSON.positions[positionIndex].players.findIndex((player) => {
+          return player.id === playerId;
         });
+        if (exists === -1) {
+          playerJSON.positions[positionIndex].players.push({
+            name: player,
+            id: playerId,
+            team: team.toUpperCase(),
+            dateAdded: new Date()
+          });
+          console.log('----------------------');
+          console.log(player + ' from ' + team.toUpperCase() + ' added.');
+        }
       }
     });
   }).then(response => {
-    fs.writeFile("player-data.json", JSON.stringify(playerJSON, null, 2), 'utf8', function (err) {
+    fs.writeFile('player-data.json', JSON.stringify(playerJSON, null, 2), 'utf8', function (err) {
       if (err) {
-        console.log("An error occured while writing JSON Object to File.");
+        console.log('An error occured while writing JSON Object to File.');
         return console.log(err);
       }
-      console.log("JSON file has been saved.");
+      console.log('JSON file has been saved.');
     });
   });
 }
